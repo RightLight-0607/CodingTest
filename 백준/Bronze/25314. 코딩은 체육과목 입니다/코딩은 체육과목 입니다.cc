101afd60b10ee4a3d4beea09fe7a9e67a53f4d5c
RightLight-0607 <160305138+RightLight-0607@users.noreply.github.com>
2024-02-17 16:44:28 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int byte, count;
+    scanf("%d", &byte);
+    count = byte / 4;
+    for (int i = 0; i < count; i++)
+    {
+        printf("long ");
+    }
+    printf("int");
+
+    return 0;
+}
