b62bab617d99b3ed98e7538a7ccc8035a07ac3bf
RightLight-0607 <160305138+RightLight-0607@users.noreply.github.com>
2024-02-17 16:43:38 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 352 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int count, num1, num2;
+    scanf("%d", &count);
+    for (int i = 0; i < count; i++)
+    {
+        scanf("%d %d", &num1, &num2);
+        printf("%d \n", num1 + num2);
+    }
+    return 0;
+}
