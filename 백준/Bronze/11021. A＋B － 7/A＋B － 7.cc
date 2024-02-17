36ad53d3c69361b5dd5a3d121ee3a2984f51422c
RightLight-0607 <160305138+RightLight-0607@users.noreply.github.com>
2024-02-17 16:42:38 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int count, num1, num2;
+    scanf("%d", &count);
+    for (int i = 0; i < count; i++)
+    {
+        scanf("%d %d", &num1, &num2);
+        printf("Case #%d: %d \n", i + 1, num1 + num2);
+    }
+    return 0;
+}
